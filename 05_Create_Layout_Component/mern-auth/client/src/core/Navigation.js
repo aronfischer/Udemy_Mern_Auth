0351f081038149c3312bed019c69aae58538f7b8
aronfischer <aronfischersigl@googlemail.com>
2020-06-11 11:34:23 +0200

    add 05_Create_Layout_Component start

---
+++ 05_Create_Layout_Component/mern-auth/client/src/core/Navigation.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+import "../styles/navigation.css";
+
+const Navigation = ({ match }) => {
+  const isActive = (urlPath) => {
+    if (match.path === urlPath) {
+      return { color: "rgba(255,255,255,0.3)" };
+    }
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark af-navbar">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          My Brand
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav w-100">
+            <li className="nav-item mr-auto">
+              <Link to="/" style={isActive("/")} className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <button type="button" className="nav-link">
+                Login
+              </button>
+            </li>
+
+            <li className="nav-item">
+              <button type="button" className="nav-link">
+                Sign up
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default withRouter(Navigation);
