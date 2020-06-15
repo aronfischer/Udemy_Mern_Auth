98fbe0b26683d6c4296f3e9f645e392bc5b8fbc3
aronfischer <aronfischersigl@googlemail.com>
2020-06-15 19:43:19 +0200

    add 11_Create_Notification_Component finished

--- 11_Create_Notification_Component/mern-auth/client/src/data/notification.js
+++ 11_Create_Notification_Component/mern-auth/client/src/data/notification.js
@@ -1,7 +1,7 @@
 export const defaultSettings = {
   container: "top-right",
   animationIn: ["animate__animated", "animate__zoomIn"],
-  animationOut: ["animate__animated", "animate__sideOutRight"],
+  animationOut: ["animate__animated", "animate__slideOutRight"],
   dismiss: {
     duration: 5000,
     onScreen: true,
