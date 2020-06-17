5cdf2b2b1d8c24c4a6839eb32cc001b4bd04e82d
aronfischer <aronfischersigl@googlemail.com>
2020-06-17 08:09:25 +0200

    add 15_Apply_App_Middleware finished

--- 15_Apply_App_Middleware/mern-auth/server/server.js
+++ 15_Apply_App_Middleware/mern-auth/server/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 // Intitialize our app
 const app = express();
@@ -6,11 +9,18 @@ const app = express();
 // Import routes
 const authRoutes = require("./routes/auth");
 
+// App middleware
+app.use(bodyParser.json());
+// app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: "http://localhost:3000" }));
+}
+
 // Apply middleware
 app.use("/api", authRoutes);
 
 // App listens to
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
