ea5698fe06d1f19af76b32850366539339bf6408
aronfischer <aronfischersigl@googlemail.com>
2020-06-19 08:20:28 +0200

    add 18_Create_User_Model_02 finished

--- 18_Create_User_Model_02/mern-auth/server/models/user.js
+++ 18_Create_User_Model_02/mern-auth/server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const crypto = require("crypto");
 
 // User Schema
 const UserSchema = new Schema(
@@ -16,6 +17,31 @@ const UserSchema = new Schema(
   }
 );
 
-// Virtuals
+// Virtual
+UserSchema.virtual("password")
+  .set(function (password) {
+    this._password = password;
+    (this.salt = this.createSalt()),
+      (this.hashed_password = this.encryptPassword(password));
+  })
+  .get(function () {
+    return this._password;
+  });
 
 // Methods
+// Create stalt: Return a random string, which adds additional security to hased_password
+UserSchema.methods.createSalt = function () {
+  return crypto.randomBytes(16).toString("hex");
+};
+
+// Encrypt user password, returns hashed_password
+UserSchema.methods.encryptPassword = function (password) {
+  return crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, "sha512")
+    .toString("hex");
+};
+
+// Authenticate user: Return true or false
+UserSchema.methods.authenticate = function (password) {
+  return encryptPassword(password) === this.hashed_password;
+};
